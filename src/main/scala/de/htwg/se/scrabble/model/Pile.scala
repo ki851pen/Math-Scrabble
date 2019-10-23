9f2a756a5623de9e510d1869539895899c9a8dee
ki851pen <pum.pensuwan@gmail.com>
2019-10-23 18:40:01 +0200

    change alphabet to mathtile

--- src/main/scala/de/htwg/se/scrabble/model/Pile.scala
+++ src/main/scala/de/htwg/se/scrabble/model/Pile.scala
@@ -4,4 +4,6 @@ case class Pile(){
   //def this(equal:Int,plus:Int,minus:Int,mul:Int,div:Int,blank:Int,digit:Int)
   val tilepile: List[Any] = List.fill(20)("="):::List.fill(7)("+"):::List.fill(7)("-"):::List.fill(5)("*"):::List.fill(5)("/"):::List.fill(6)("_"):::List.fill(5)((0 to 9).toList).flatten
   def size: Int = tilepile.length
+
+  override def toString: String = tilepile.toString()
 }
--- src/test/scala/de/htwg/se/scrabble/model/PileSpec.scala
+++ src/test/scala/de/htwg/se/scrabble/model/PileSpec.scala
@@ -11,6 +11,9 @@ class PileSpec extends WordSpec with Matchers {
       "have length of 100" in{
         newpile.size should be (100)
       }
+      "have a String representation" in{
+        newpile.toString should be ("List(=, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, +, +, +, +, +, +, +, -, -, -, -, -, -, -, *, *, *, *, *, /, /, /, /, /, _, _, _, _, _, _, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)")
+      }
     }
   }
 }
