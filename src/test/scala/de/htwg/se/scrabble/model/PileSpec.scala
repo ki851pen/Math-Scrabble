c73205dc757a5e6628c82e5201286408c027df78
ki851pen <pum.pensuwan@gmail.com>
2019-11-03 19:41:06 +0100

    edit pilespec

--- src/test/scala/de/htwg/se/scrabble/model/PileSpec.scala
+++ src/test/scala/de/htwg/se/scrabble/model/PileSpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.{Matchers, WordSpec}
 class PileSpec extends WordSpec with Matchers {
   "A Pile" when {
     "create by defaulft" should{
-      val newpile = Pile()
+      val newpile = new Pile()
       "have a list" in {
         newpile.tilepile shouldBe a [List[_]]
       }
@@ -12,16 +12,16 @@ class PileSpec extends WordSpec with Matchers {
         newpile.size should be (100)
       }
       "have a String representation" in{
-        newpile.toString should be ("List(=, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, +, +, +, +, +, +, +, -, -, -, -, -, -, -, *, *, *, *, *, /, /, /, /, /, _, _, _, _, _, _, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)")
+        newpile.toString should be ("=, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, =, +, +, +, +, +, +, +, -, -, -, -, -, -, -, *, *, *, *, *, /, /, /, /, /, _, _, _, _, _, _, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9")
       }
     }
     "shuffle" should{
-      val pile = Pile()
+      val pile = new Pile()
       "have same length" in {
         pile.shuffle.tilepile.length should be (100)
       }
       "have a different String representation" in {
-        pile.shuffle.toString should not be ("")
+        pile.shuffle.toString should not be pile.toString
       }
     }
   }
