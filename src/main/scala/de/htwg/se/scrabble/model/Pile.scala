e835f8a420ef5cc594b288a46c1b9174de74cd41
ki851pen <pum.pensuwan@gmail.com>
2019-11-03 19:37:12 +0100

    remake pile

--- src/main/scala/de/htwg/se/scrabble/model/Pile.scala
+++ src/main/scala/de/htwg/se/scrabble/model/Pile.scala
@@ -1,13 +1,10 @@
 package de.htwg.se.scrabble.model
 import scala.util.Random
 //case class Pile(mode: Int){
-case class Pile(){
-  val tilepile: List[Any] = List.fill(20)("="):::List.fill(7)("+"):::List.fill(7)("-"):::List.fill(5)("*"):::List.fill(5)("/"):::List.fill(6)("_"):::List.fill(5)((0 to 9).toList).flatten
+case class Pile(private val tilepile: List[Any]){
+  def this() = this(List.fill(20)("="):::List.fill(7)("+"):::List.fill(7)("-"):::List.fill(5)("*"):::List.fill(5)("/"):::List.fill(6)("_"):::List.fill(5)((0 to 9).toList).flatten)
   def size: Int = tilepile.length
-  def shuffle:Pile = Pile(Random.shuffle(tilepile))
+  def shuffle: Pile = Pile(Random.shuffle(tilepile))
 
   override def toString: String = tilepile.mkString(", ")
-  case class Pile(value: List[Any]){
-    val tilepile = value
-  }
 }
