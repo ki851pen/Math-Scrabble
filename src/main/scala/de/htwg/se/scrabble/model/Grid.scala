ea570ce4aaaad1c3ce21c29402c8122f58f5b30e
ki851pen <pum.pensuwan@gmail.com>
2019-11-05 22:38:13 +0100

    combine matrix and grid

--- src/main/scala/de/htwg/se/scrabble/model/Grid.scala
+++ src/main/scala/de/htwg/se/scrabble/model/Grid.scala
@@ -1,14 +1,16 @@
 package de.htwg.se.scrabble.model
 
-case class Grid(private val cells:Matrix[Cell]) {
-  def this(size:Int) = this(new Matrix[Cell](size, new Cell("")))
+case class Grid(private val cells:Vector[Vector[Cell]]) {
+  def this(size:Int) = this(Vector.tabulate(size,size){(row,col) => new Cell("")})
   val size:Int = cells.size
-  def cell(row:Int, col:Int):Cell = cells.cell(row, col)
-  def isEmpty: Boolean = cells.rows.forall(c => c.forall(d => d.value == ""))
-  def checkNeighbor(row:Int, col:Int): List[String] = cells.rows(row-1)(col).value :: cells.rows(row)(col-1).value :: cells.rows(row+1)(col).value :: cells.rows(row)(col+1).value :: Nil
-  def emptyNeighbor(row:Int, col:Int): Boolean = checkNeighbor(row,col).forall(s => s ==" ")
+  def cell(row:Int, col:Int):Cell = cells(row)(col)
+  def fill(filling: Cell): Grid = copy(Vector.tabulate(size, size) { (row, col) => filling})
+  def isEmpty: Boolean = cells.forall(v => v.forall(c => c.isSet))
+
+  def checkNeighbor(row:Int, col:Int): List[String] = cells(row-1)(col).value :: cells(row)(col-1).value :: cells(row+1)(col).value :: cells(row)(col+1).value :: Nil
+  def emptyNeighbor(row:Int, col:Int): Boolean = checkNeighbor(row,col).forall(s => s =="")
   //TODO: man kann cell nur setzen wenn eine Nachbarn Cell ein Wert besitzt
-  def set(row:Int, col:Int, value:String):Grid = copy(cells.replaceCell(row, col, Cell(value, cell(row, col).special)))
+  def set(row:Int, col:Int, value:String):Grid = copy(cells.updated(row, cells(row).updated(col, Cell(value, cells(row)(col).special))))
 
   override def toString: String = {
     val numCol = "      " + List.range(1, size + 1).filter(_<10).mkString("    ") + "    " + List.range(1, size + 1).filter(_>9).mkString("   ") +  "  \n"
