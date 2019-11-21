e67cbd7a0d1908bfb35aff84135445a9cd48b9d3
ki851pen <pum.pensuwan@gmail.com>
2019-11-21 09:12:58 +0100

    update tuispec

--- src/test/scala/de/htwg/se/scrabble/aview/TuiSpec.scala
+++ src/test/scala/de/htwg/se/scrabble/aview/TuiSpec.scala
@@ -11,7 +11,33 @@ class TuiSpec extends WordSpec with Matchers{
       tui.processInputLine("g")
       controller.game.grid should be(new Grid(15))
     }
+    "create 5x5 empty Grid on input 'g 5'" in {
+      tui.processInputLine("g 5")
+      controller.game.grid should be(new Grid(5))
+    }
+    "create a default pile on input 'p'" in {
+      tui.processInputLine("p")
+      controller.game.pile.size should be(100)
+      controller.game.pile.toString should (include("+") and include("-") and  include("*") and include("/") and include("=") and include("_") and include("5"))
+    }
+    "shuffle a pile on input 's'" in {
+      val oldpile = controller.game.pile.tilepile
+      tui.processInputLine("s")
+      controller.game.pile.size should be(100)
+      controller.game.pile.tilepile should not equal oldpile
+    }
+    "add player B on input 'py+ B'" in {
+      tui.processInputLine("py+ B")
+      controller.game.playerList.keys should contain ("B")
+      controller.game.playerList.size should be (2)
+    }
+    "remove player B on input 'py- B'" in {
+      tui.processInputLine("py- B")
+      controller.game.playerList.keys should not contain "B"
+      controller.game.playerList.size should be (1)
+    }
     "set a cell on input '8 8 3'" in {
+      tui.processInputLine("g")
       tui.processInputLine("8 8 3")
       controller.game.grid.cell(7,7).value should be("3")
     }
