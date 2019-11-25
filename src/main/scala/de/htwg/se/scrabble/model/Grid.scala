1f7c5c7cb73e986448a7df9906fbc629adc58e7a
Carolin König <carolin.koenig@htwg-konstanz.de>
2019-11-25 19:46:02 +0100

    check if neighbor of cell already has a value, if not set is not possible and a new cell has to be selected

--- src/main/scala/de/htwg/se/scrabble/model/Grid.scala
+++ src/main/scala/de/htwg/se/scrabble/model/Grid.scala
@@ -9,8 +9,35 @@ case class Grid(private val cells:Vector[Vector[Cell]]) {
 
   //def checkNeighbor(row:Int, col:Int): List[String] = cells(row-1)(col).value :: cells(row)(col-1).value :: cells(row+1)(col).value :: cells(row)(col+1).value :: Nil
   //def emptyNeighbor(row:Int, col:Int): Boolean = checkNeighbor(row,col).forall(s => s =="")
-  //TODO: man kann cell nur setzen wenn eine Nachbarn Cell ein Wert besitzt
-  def set(row:Int, col:Int, value:String):Grid = copy(cells.updated(row, cells(row).updated(col, Cell(value, cells(row)(col).special))))
+
+  def checkNeighbor(row:Int, col:Int): Boolean = {
+    if (row != 0 && cells(row - 1)(col).isSet) {
+      true
+    }
+    else if (col != 0 && cells(row)(col - 1).isSet) {
+      true
+    }
+    else if (row != this.size - 1 && cells(row + 1)(col).isSet) {
+      true
+    }
+    else if (col != this.size - 1 && cells(row)(col + 1).isSet) {
+      true
+    }
+    else {
+      false
+    }
+  }
+
+
+  def set(row:Int, col:Int, value:String):Grid =  {
+    if (!checkNeighbor(row, col) && !cells.forall(v => v.forall(c => !c.isSet))) {
+      println("\nSteine koennen nur horizontal oder vertikal neben einem bereits auf dem Spielplan liegenden Stein platziert werden!\n" +
+        "Bitte geben Sie eine neue Position an!\n")
+      copy()
+    } else {
+      copy(cells.updated(row, cells(row).updated(col, Cell(value, cells(row)(col).special))))
+    }
+  }
 
   override def toString: String = {
     val numCol = "      " + List.range(1, size + 1).filter(_<10).mkString("     ") + "    " + List.range(1, size + 1).filter(_>9).mkString("    ") +  "  \n"
