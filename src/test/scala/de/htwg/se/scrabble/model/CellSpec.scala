bcff21157ce381c7bfb83dae70af021a59edc5e3
ki851pen <pum.pensuwan@gmail.com>
2019-11-28 00:48:28 +0100

    complete cellspec

--- src/test/scala/de/htwg/se/scrabble/model/CellSpec.scala
+++ src/test/scala/de/htwg/se/scrabble/model/CellSpec.scala
@@ -20,15 +20,38 @@ class CellSpec extends WordSpec with Matchers {
         testcell2.cellType shouldBe "d"
         testcell3.cellType shouldBe "t"
       }
+      "have a String representation" in {
+        testcell.toString shouldBe " "
+        testcell2.toString shouldBe "x2"
+        testcell3.toString shouldBe "x3"
+      }
     }
     "set correctly" should {
       val testcell = Cell("")
+      val testdoublecell = Cell("d","")
+      val testtripplecell = Cell("t","")
       val changedcell = testcell.setCell("1")
+      val changeddoublecell = testdoublecell.setCell("7")
+      val changedtripplecell = testtripplecell.setCell("?")
       "have changed value" in {
         changedcell.card shouldBe Card("1")
+        changeddoublecell.card shouldBe Card("7")
+        changedtripplecell.card shouldBe Card("?")
+      }
+      "be set" in {
+        changedcell.isSet shouldBe true
+        changeddoublecell.isSet shouldBe true
+        changedtripplecell.isSet shouldBe true
       }
       "have a point assign to them" in {
         changedcell.getPoint shouldBe 1
+        changeddoublecell.getPoint shouldBe 8
+        changedtripplecell.getPoint shouldBe 0
+      }
+      "have a String representation" in {
+        changedcell.toString shouldBe "1"
+        changeddoublecell.toString shouldBe "7x2"
+        changedtripplecell.toString shouldBe "?x3"
       }
     }
     "set incorrectly" should {
@@ -37,8 +60,22 @@ class CellSpec extends WordSpec with Matchers {
       "have old value" in {
         changedcell.card shouldBe Card("")
       }
+      "not be set" in {
+        changedcell.isSet shouldBe false
+      }
       "have a point assign to them" in {
         changedcell.getPoint shouldBe 0
+      }
+    }
+    "check equality" should {
+      val testcell = Cell("")
+      val testcell2 = Cell("n","")
+      val testcell3 = Cell("d","")
+      "be equal with same type" in {
+        testcell shouldEqual testcell2
+      }
+      "not equal with different type" in {
+        testcell should not equal testcell3
       }
     }
   }
