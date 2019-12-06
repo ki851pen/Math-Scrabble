845a0faed8aae0f6c79030aa973e244d17784f12
ki851pen <pum.pensuwan@gmail.com>
2019-12-06 18:13:09 +0100

    remove parentheses after getGameField

--- src/test/scala/de/htwg/se/scrabble/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/scrabble/controller/ControllerSpec.scala
@@ -20,9 +20,9 @@ class ControllerSpec extends WordSpec with Matchers{
         observer.reset()
         controller.init()
         observer.updated should be(true)
-        controller.getGameField().grid.size shouldBe 15
-        controller.getGameField().playerList("A").getNrCardsInHand should not be 0
-        controller.getGameField().playerList("B").getNrCardsInHand should not be 0
+        controller.getGameField.grid.size shouldBe 15
+        controller.getGameField.playerList("A").getNrCardsInHand should not be 0
+        controller.getGameField.playerList("B").getNrCardsInHand should not be 0
         controller.gameStatus = GameStatus.P("A")
       }
       "notify its Observer after calculate point" in {
@@ -39,14 +39,14 @@ class ControllerSpec extends WordSpec with Matchers{
         observer.reset()
         controller.createFixedSizeGameField(3)
         observer.updated should be(true)
-        controller.getGameField().grid.size should be(3)
-        controller.gameToString should be(controller.getGameField().gameToString("A"))
+        controller.getGameField.grid.size should be(3)
+        controller.gameToString should be(controller.getGameField.gameToString("A"))
       }
       "notify its Observer after grid creation with free size" in {
         observer.reset()
         controller.createFreeSizeGameField(4, 1, 1, 1, 1, 1)
         observer.updated should be(true)
-        controller.getGameField().grid.size should be(4)
+        controller.getGameField.grid.size should be(4)
       }
       "notify its Observer after setting a first cell in middle" in {
         observer.reset()
@@ -56,7 +56,7 @@ class ControllerSpec extends WordSpec with Matchers{
         controller.fillHand("A")
         controller.setGrid(2,2,"=")
         observer.updated should be(true)
-        controller.getGameField().grid.cell(2,2) shouldEqual Cell("t","=")//in input is already + 1
+        controller.getGameField.grid.cell(2,2) shouldEqual Cell("t","=")//in input is already + 1
       }
       "not notify its Observer after setting a first cell not in middle" in {
         observer.reset()
@@ -64,28 +64,28 @@ class ControllerSpec extends WordSpec with Matchers{
         observer.reset()
         controller.setGrid(1,1,"4")
         observer.updated should be(false)//<-- not pass
-        controller.getGameField().grid.cell(1,1) shouldEqual Cell("d","")
+        controller.getGameField.grid.cell(1,1) shouldEqual Cell("d","")
       }
       "notify its Observer after pile creation" in {
         observer.reset()
         controller.createPile(5, 1, 1, 1, 1)
         observer.updated should be(true)
-        controller.getGameField().pile.size should be(20)
+        controller.getGameField.pile.size should be(20)
       }
       "notify its Observer after shuffle the pile" in {
         observer.reset()
-        val oldgame = controller.getGameField()
+        val oldgame = controller.getGameField
         controller.shufflePile()
         observer.updated should be(true)
-        controller.getGameField().pile should not be oldgame.pile
+        controller.getGameField.pile should not be oldgame.pile
       }
       "notify its Observer after existing player take cards from pile" in {
         observer.reset()
-        val oldgame = controller.getGameField()
+        val oldgame = controller.getGameField
         controller.fillHand("A")
         observer.updated should be(true)
-        controller.getGameField().playerList("A").getNrCardsInHand should be (9)
-        controller.getGameField().pile.size should be (oldgame.pile.size - 9)
+        controller.getGameField.playerList("A").getNrCardsInHand should be (9)
+        controller.getGameField.pile.size should be (oldgame.pile.size - 9)
       }
       "not notify its Observer after player that not in the list try take cards from pile" in {
         observer.reset()
@@ -96,15 +96,15 @@ class ControllerSpec extends WordSpec with Matchers{
         observer.reset()
         controller.fillAllHand
         observer.updated should be(true)
-        controller.getGameField().playerList("B").getNrCardsInHand should be (9)
+        controller.getGameField.playerList("B").getNrCardsInHand should be (9)
       }
       "notify its Observer after clear a player hand" in {
-        val oldPileSize: Int = controller.getGameField().pile.size
+        val oldPileSize: Int = controller.getGameField.pile.size
         observer.reset()
         controller.clearHand("B")
         observer.updated should be(true)
-        controller.getGameField().playerList("B").getNrCardsInHand should be (0)
-        controller.getGameField().pile.size should be (oldPileSize + 9)
+        controller.getGameField.playerList("B").getNrCardsInHand should be (0)
+        controller.getGameField.pile.size should be (oldPileSize + 9)
       }
     }
   }
