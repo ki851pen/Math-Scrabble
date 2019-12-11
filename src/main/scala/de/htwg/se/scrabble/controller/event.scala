dfc7689f7127f6d3d647a801b720d6ac08c73566
ki851pen <pum.pensuwan@gmail.com>
2019-12-11 12:07:43 +0100

    add events

---
+++ src/main/scala/de/htwg/se/scrabble/controller/event.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.cards.controller
+
+import scala.swing.event.Event
+
+class StatusChanged extends Event
+class CardsChanged extends Event
+class PlayerChanged extends Event
+class PileChanged extends Event
+class GridChanged extends Event
