ad8bf646e828ed11022c3c7d34891d01c2c1c5bf
ki851pen <pum.pensuwan@gmail.com>
2019-12-12 19:44:53 +0100

    move customcolors from testbranch

---
+++ src/main/scala/de/htwg/se/scrabble/util/CustomColors.scala
@@ -0,0 +1,12 @@
+package de.htwg.se.scrabble.util
+
+import scala.swing.Color
+
+object CustomColors {
+  val Blue = new Color(50, 100, 200)
+  val Green = new Color(10, 255, 100)
+  val LightBlue = new Color(0, 230, 255)
+  val White = new Color(255, 255, 255)
+  val Red = new Color(255, 60, 60)
+  val Yellow = new Color(255, 250, 80)
+}
