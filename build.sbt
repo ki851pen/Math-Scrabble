f3d072bdced28919d0d56b746245dfb2fda04522
ki851pen <pum.pensuwan@gmail.com>
2020-01-21 20:40:20 +0100

    finish processEquation

--- build.sbt
+++ build.sbt
@@ -12,4 +12,4 @@ libraryDependencies += "net.codingwell" %% "scala-guice" % "4.2.6"
 libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.2.0"
 libraryDependencies += "com.typesafe.play" %% "play-json" % "2.8.1"
 
-libraryDependencies += "com.github.pdorobisz" %% "math-expression-evaluator" % "1.0.0"
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
--- src/main/scala/de/htwg/se/scrabble/controller/controllerComponent/ProcessEquation.scala
+++ src/main/scala/de/htwg/se/scrabble/controller/controllerComponent/ProcessEquation.scala
@@ -5,10 +5,29 @@ import de.htwg.se.scrabble.model.gridComponent.CellInterface
 
 import collection.mutable.ListBuffer
 
-import com.github.pdorobisz.mathexpressionevaluator.Evaluator
-import com.github.pdorobisz.mathexpressionevaluator.errors.EvaluatorError
+import scala.util.parsing.combinator.RegexParsers
 
-import scala.util.{Failure, Success}
+object Calculator extends RegexParsers {
+  def number: Parser[Double] = """\d+(\.\d*)?""".r ^^ { _.toDouble }
+  def factor: Parser[Double] = number | "(" ~> expr <~ ")"
+  def term  : Parser[Double] = factor ~ rep( "*" ~ factor | "/" ~ factor) ^^ {
+    case number ~ list => (number /: list) {
+      case (x, "*" ~ y) => x * y
+      case (x, "/" ~ y) => x / y
+    }
+  }
+  def expr  : Parser[Double] = term ~ rep("+" ~ log(term)("Plus term") | "-" ~ log(term)("Minus term")) ^^ {
+    case number ~ list => list.foldLeft(number) { // same as before, using alternate name for /:
+      case (x, "+" ~ y) => x + y
+      case (x, "-" ~ y) => x - y
+    }
+  }
+
+  def apply(input: String): Double = parseAll(expr, input) match {
+    case Success(result, _) => result
+    case failure : NoSuccess => scala.sys.error(failure.msg)
+  }
+}
 
 case class ProcessEquation(controller: ControllerInterface) {
   var newcell: List[(Int, Int)] = Nil
@@ -52,7 +71,7 @@ case class ProcessEquation(controller: ControllerInterface) {
   }
 
   def evalEquation(equation: Seq[CellInterface]): Boolean = {
-    var valList: List[Int] = Nil
+    var valList: List[Double] = Nil
     val operator = Seq("+", "-", "*", "/")
     val strEqa: Seq[String] = equation.map(c => c.card.toString)
     if (!strEqa.contains("=")) {
@@ -61,12 +80,15 @@ case class ProcessEquation(controller: ControllerInterface) {
       val expList = splitBySeparator(strEqa, "=")
       expList.foreach(exp => {
         if (exp.exists(c => operator.exists(_ contains c))) {
-          Evaluator.evaluate(exp.mkString("")) match {
-            case Success(s) => valList = s :: valList
-            case Failure(_) => return false
+          Calculator(exp.mkString("")) match {
+            case x :Double => {
+              valList = x :: valList
+              println(valList)
+            }
+            case _ => return false
           }
         } else {
-          valList = exp.mkString("").toInt :: valList
+          valList = exp.mkString("").toDouble :: valList
           println(valList)
         }
       })
